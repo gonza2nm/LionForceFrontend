c0b03b2a2751d5a133a4a54a0f0f9a6dd778afe6
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-10-28 23:13:11 -0300

    arregle problema con el navbar ya que solo se veia en una sola pagina

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,11 +2,14 @@
 import { ReactNode } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthContextProvider from '../hooks/authContext.tsx';
+import NavBar from '../components/NavBar.tsx';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <AuthContextProvider>{children}</AuthContextProvider>
+      <AuthContextProvider>
+        <NavBar>{children}</NavBar>
+      </AuthContextProvider>
     </div>
   );
 }
